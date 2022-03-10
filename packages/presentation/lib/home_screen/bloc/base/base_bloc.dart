c31b18884f5a38e373c5e9945a8209d06ea4b01e
alexei-kruk-idf <alexei.kruk@idfinance.com>
2022-03-10 13:59:09 +0300

    add bloc

---
+++ packages/presentation/lib/home_screen/bloc/base/base_bloc.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+import 'package:presentation/home_screen/bloc/base/bloc_data.dart';
+
+abstract class BaseBloc<D> {
+  Stream<BlocData<D?>> get dataStream;
+
+  void initState();
+
+  void dispose();
+}
+
+class BlocIml<D> implements BaseBloc<D> {
+  @override
+  Stream<BlocData<D?>> get dataStream => _data.stream;
+  final _blocData = BlocData.init();
+  final _data = StreamController<BlocData<D?>>();
+
+  @override
+  void dispose() {
+    _data.close();
+  }
+
+  @protected
+  void handleData({
+    bool? isLoading,
+    bool? isValid,
+    D? data,
+  }) {
+    if (!_data.isClosed) {
+      _blocData.updateParams(
+        isLoading: isLoading,
+        data: data,
+      );
+      _data.add(_blocData.copy<D>());
+    }
+  }
+
+  @override
+  void initState() {}
+}
